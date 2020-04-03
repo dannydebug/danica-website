05b6f87efd90830e548c4f847e8c6d706b836077
dannydebug <danica.v90@gmail.com>
2020-04-03 17:18:48 -0300

    Initial commit of website built with Parcel

---
+++ src/home.js
@@ -0,0 +1,55 @@
+import $ from "jquery";
+import "bootstrap";
+import "waypoints/lib/noframework.waypoints";
+
+// CSS
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
+
+$(document).ready(() => {
+  setupMouseAnimation();
+  setupSmoothScroll();
+  waypointCheck();
+});
+
+function generateGradient(x, y) {
+  return `radial-gradient(at bottom left, rgba(46,158,183,1) ${x *
+    25}%, rgba(49,25,109,1) 100%)`;
+}
+
+function setupMouseAnimation() {
+  const el = $(".welcome-header");
+  el.on("mousemove", e => {
+    const x = e.clientX / el.width();
+    const y = e.clientY / el.height();
+    el.css("background", generateGradient(x, y));
+  });
+}
+
+function setupSmoothScroll() {
+  const anchors = document.querySelectorAll('a[href^="#"]');
+  for (const a of anchors) {
+    a.addEventListener("click", function(event) {
+      event.preventDefault();
+      const target = document.querySelector(this.hash);
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  }
+}
+
+function waypointCheck() {
+  const waypoint = new Waypoint({
+    element: document.getElementById("about-me"),
+    offset: "50%",
+    handler: function() {
+      const elements1 = this.element.getElementsByClassName("offscreen-left");
+      const elements2 = this.element.getElementsByClassName("offscreen-right");
+      for (const el of [...elements1, ...elements2]) {
+        el.classList.remove("offscreen-left", "offscreen-right");
+      }
+    }
+  });
+}
